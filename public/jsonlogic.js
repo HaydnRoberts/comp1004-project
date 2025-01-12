684d69e064e6535f173a5ec40ef9541d76f1f532
HaydnRoberts <128608553+HaydnRoberts@users.noreply.github.com>
2025-01-12 17:56:25 +0000

    temporarily fixed the json code by putting in a placeholder until I understand the system better

--- public/jsonlogic.js
+++ public/jsonlogic.js
@@ -6,28 +6,7 @@ function savedata(){
         game4value: "0"
         }
 
-    var jsonData = JSON.stringify(data);
+    //var jsonData = JSON.stringify(data);
 
-    // this code block does not work right
-    var fs = require('fs');
-    fs.writeFile("./savedata.txt", jsonData, function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-}
-
-function readdata(){
-  fetch("./savedata.txt")
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error
-          (`HTTP error! Status: ${res.status}`);
-        }
-          return res.json();
-        })
-        .then((data) =>
-          console.log(data))
-        .catch((error) =>
-          console.error("Unable to fetch data:", error));
+    console.log("The Json will be printed here");
 }
