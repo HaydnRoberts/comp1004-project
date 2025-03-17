eb3dac632a65509ecaaa45416fd205dbf918db08
HaydnRoberts <128608553+HaydnRoberts@users.noreply.github.com>
2025-03-17 13:54:01 +0000

    Added personally made icons for some monsters, and set groundworks for monster selection

--- public/turnbasedgame.js
+++ public/turnbasedgame.js
@@ -11,6 +11,10 @@ const enemyHealthGUI = document.querySelector('#enemy-health');
 const enemyImageGUI = document.querySelector('#enemy-image');
 
 const combatGUI = document.querySelector('#game-3-combat');
+const typeGUI = document.querySelector('#game-3-type-choice');
+const subclassGui = document.querySelector('#game-3-subclass-choice')
+const subclassChoice0 = document.querySelector('#subclass0');
+const subclassChoice1 = document.querySelector('#subclass1');
 
 function HealthBar(progression, end, length){
 	let progressBar = "";
@@ -31,7 +35,47 @@ function HealthBar(progression, end, length){
 	return progressBar;
 }
 
-let Types = ["Fire", "Ice", "Healer", "Poison", "Ground", "Flying", "Steel", "Electric"]
-let subclasses = ["Flame", "Molten", "Freeze", "Slow", "Medic", "Vamiric", "Venom", "Viral", "Sandstorm", "Flora", "Breeze", "Clense", "Armour", "Blade", "Grounded", "Charge"];
+let Types = ["Fire", "Ice", "Healer", "Poison", "Ground", "Flying", "Steel", "Electric"];
+let subclasses = ["Flame", "Molten", "Freeze", "Slow", "Medic", "Vampiric", "Venom", "Viral", "Sandstorm", "Flora", "Breeze", "Clense", "Armour", "Blade", "Grounded", "Charge"];
 // items are the equivelant of a craftable second subclass
 // Type 1 can access subclass 1 and 2, Type 2 can access subclass 3 and 3, ect
+
+let playerMonster = ['','','']; // 0 is type, 1 is subclass, 2 is item
+let tmp = 0;
+function chooseType(value){
+	playerMonster[0] = Types[value];
+	tmp = value;
+
+	subclassChoice0.innerHTML = subclasses[tmp*2];
+	subclassChoice1.innerHTML = subclasses[(tmp*2)+1];
+	
+	typeGUI.classList.add('hidden');
+	subclassGui.classList.remove('hidden');
+}
+function subclass(value){
+	playerMonster[1] = subclasses[((tmp*2)+value)];
+
+	playerTypeGUI.innerHTML = playerMonster[0];
+	playerSubclassGUI.innerHTML = playerMonster[1];
+	playerItemGUI.innerHTML = "no item";
+	playerHealthGUI.innerHTML = HealthBar(100,100,50);
+
+	subclassGui.classList.add('hidden');
+	combatGUI.classList.remove('hidden');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+typeGUI.classList.remove('hidden');
+subclassGui.classList.add('hidden');
+combatGUI.classList.add('hidden');
